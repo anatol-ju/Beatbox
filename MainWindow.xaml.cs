bd6362391d4a327c6a1f3feaa8f4382198406392
azurius-coding <55158774+azurius-coding@users.noreply.github.com>
2020-10-20 20:53:52 +0200

    Finished work on base functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Beatbox
 {
@@ -25,11 +14,9 @@ namespace Beatbox
     {
         private static BackgroundWorker worker;
 
-        private static int minDamageValueForLevel = 0;
-        private static int maxDamageValueForLevel = 10;
+        private static int maxDamageValueForLevel = 100;
 
-        private static int minDamage = 2;
-        private static int maxDamage = 5;
+        private static int damagePerHit = 10;
         private static int baseAttackRate = 2500;       // in millisec
         private static double baseCritChance = 5.0;
 
@@ -38,13 +25,17 @@ namespace Beatbox
         private static int currentAP = 1;
         private static int currentCR = 1;
         private static int currentHR = 1;
-        private static int currentMinDamage = minDamage;
-        private static int currentMaxDamage = maxDamage;
+        private static int currentDamagePerHit = damagePerHit;
         private static int currentAttackRate = baseAttackRate;
+        private static double currentCritChance = 5.0;
+        private static int currentRecord = 0;
 
-        private static double convertRatioAP = 1.41;    // increases the damage
+        private static int availablePoints = 0;
+        private static int overdraft = 0;
+
+        private static double convertRatioAP = 1.15;    // increases the damage
         private static double convertRatioCR = 1.1;     // increases crit chance
-        private static double convertRatioHR = 1.05;    // decreases attack rate
+        private static double convertRatioHR = 1.1;     // decreases attack rate
 
         public MainWindow()
         {
@@ -55,11 +46,12 @@ namespace Beatbox
         {
             Random random = new Random();
 
-            int returned = random.Next(currentMinDamage, currentMaxDamage + 1);
+            int minDmg = (int) (currentDamagePerHit * 0.75);
+            int returned = random.Next(minDmg, currentDamagePerHit + 1);
 
-            double critChance = baseCritChance + convertRatioCR * Math.Log(currentCR);
-            int check = random.Next(1, 101);
-            if (check <= critChance)
+            currentCritChance = baseCritChance * Math.Pow(convertRatioCR, currentCR - 1);
+            double check = random.NextDouble()*100+1;
+            if (check <= currentCritChance)
             {
                 returned *= 2;
             }
@@ -74,13 +66,16 @@ namespace Beatbox
         /// <param name="e"></param>
         private void Window_ContentRendered(object sender, EventArgs e)
         {
-            //StartWorker();
+            InitWorker();
             HideIncreaseButtons();
             // init UI
+            ValueAP.Content = currentAP;
+            ValueCR.Content = currentCR;
+            ValueHR.Content = currentHR;
             AttackRateValue.Content = currentAttackRate/1000.0;
         }
 
-        private void StartWorker()
+        private void InitWorker()
         {
             System.Diagnostics.Debug.WriteLine("Worker started...");
 
@@ -90,8 +85,6 @@ namespace Beatbox
             worker.DoWork += Worker_DoWork;
             worker.ProgressChanged += Worker_ProgressChanged;
             worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
-
-            worker.RunWorkerAsync();
         }
 
         private void Worker_RunWorkerCompleted1(object sender, RunWorkerCompletedEventArgs e)
@@ -108,22 +101,23 @@ namespace Beatbox
         {
             System.Diagnostics.Debug.WriteLine("Doing work");
             int dmg = 0;
-            int sum = Math.Max(minDamageValueForLevel, currentXP);
-            while(sum < maxDamageValueForLevel)
+            currentXP = Math.Max(0, overdraft);
+            while(currentXP < maxDamageValueForLevel)
             {
                 // if cancelation is needed, see "completed" method
                 // call worker.CancelAsync() from UI to set CancellationPending
-                if (worker.CancellationPending == true)
+                if ((sender as BackgroundWorker).CancellationPending == true)
                 {
                     e.Cancel = true;
-                    currentXP = sum;
                     return;
                 }
                 dmg = CalcDamageValue();
-                sum += dmg;
-                System.Diagnostics.Debug.WriteLine("dmg: {0}, sum: {1}", dmg, sum);
+                currentXP += dmg;
+                System.Diagnostics.Debug.WriteLine("dmg: {0}, sum: {1}", dmg, currentXP);
+                overdraft = currentXP - maxDamageValueForLevel;
                 // must be a percentage
-                worker.ReportProgress((int)(100 * sum / (double)maxDamageValueForLevel), dmg);
+                int percentage = (int)(100 * currentXP / (double)maxDamageValueForLevel);
+                (sender as BackgroundWorker).ReportProgress(percentage, dmg);
 
                 Thread.Sleep(currentAttackRate);
             }
@@ -138,96 +132,185 @@ namespace Beatbox
         {
             System.Diagnostics.Debug.WriteLine("progress changed");
             XPBar.Value = e.ProgressPercentage;
-            StringBuilder sb = new StringBuilder(Log.Text);
-            sb.Append((int)e.UserState);
-            sb.Append("\n");
-            Log.Text = sb.ToString();
+            UpdateDPS();
+            UpdateMaxDamage((int)e.UserState);
+            AppendToLog((int)e.UserState, "\n");
         }
 
         /// <summary>
-        /// Executed when the BackgroundWorker finished. This is used to update the values for the next level-up.
+        /// Executed when the BackgroundWorker finished.
+        /// When it finishes due to user input or an error, it disposes resources.
+        /// When finishing normally, it hides buttons and updates level constraints and progress bar.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             System.Diagnostics.Debug.WriteLine("Worker completed.");
-            // detects if worker was cancelled manually
-            if (e.Cancelled)
+            
+            if (e.Cancelled)    // manual cancellation
             {
-
+                //worker.CancelAsync();
+                (sender as BackgroundWorker).Dispose();
             }
-            else if (e.Error != null)
+            else if (e.Error != null)   // error occured
             {
-
+                //worker.CancelAsync();
+                (sender as BackgroundWorker).Dispose();
             }
-            // stop worker
-            worker.CancelAsync();
-            worker.Dispose();
-            // update
-            currentXP = 0;
-            ShowIncreaseButtons();
-            UpdateLevelConstraints();
-            UpdateProgressBar();
-            // restart worker on new thread
+            else    // normal continuation
+            {
+                // update calculations and UI
+                ShowIncreaseButtons();
+                UpdateLevelConstraints();
+                UpdateProgressBar();
+
+                availablePoints++;
+
+                if (overdraft > 0)
+                {
+                    currentXP = overdraft;
+                }
+                else
+                {
+                    currentXP = 0;
+                }
+
+                (sender as BackgroundWorker).RunWorkerAsync();
+            }
         }
 
+        /// <summary>
+        /// Updates attack rate with a given formula.
+        /// </summary>
         private void UpdateAttackRate()
         {
-            currentAttackRate = (int)(baseAttackRate - convertRatioHR * Math.Log(currentHR));
-            AttackRateValue.Content = currentAttackRate;
+            currentAttackRate = (int) (baseAttackRate * Math.Pow(convertRatioHR, currentHR - 1));
+            AttackRateValue.Content = currentAttackRate / 1000.0;
         }
 
+        /// <summary>
+        /// Updates constraints for min and max damage for the level-up.
+        /// </summary>
         private void UpdateLevelConstraints()
         {
             currentLevel += 1;
-            minDamageValueForLevel = maxDamageValueForLevel;
-            maxDamageValueForLevel = (int)(maxDamageValueForLevel * Math.Exp(currentLevel));
+            maxDamageValueForLevel = maxDamageValueForLevel * currentLevel;
         }
 
-        private void UpdateDamageConstraints()
+        /// <summary>
+        /// Updates minimum and maximum damage possible.
+        /// </summary>
+        private void UpdateDamagePerHit()
         {
-            currentMinDamage = (int)(minDamage * convertRatioAP * Math.Log(currentAP));
-            currentMaxDamage = (int)(maxDamage * convertRatioAP * Math.Log(currentAP));
+            currentDamagePerHit = (int) (currentDamagePerHit * convertRatioAP);
+            System.Diagnostics.Debug.WriteLine("currentMinDamage: {0}", (int)(currentDamagePerHit * 0.75));
+            System.Diagnostics.Debug.WriteLine("currentMaxDamage: {0}", currentDamagePerHit);
         }
 
+        /// <summary>
+        /// Used to update the progress bar on the UI based on new data.
+        /// Invoke only after maxDamageValueForLevel had been updated.
+        /// </summary>
         private void UpdateProgressBar()
         {
-            XPBar.Value = 0;
+            XPBar.Value = (int)(100*currentXP/(double)maxDamageValueForLevel);
         }
 
+        /// <summary>
+        /// Function to update the damage per second. It is calculated entirely on average damage
+        /// including critical hit chance.
+        /// </summary>
+        private void UpdateDPS()
+        {
+            CurrentDamageValue.Content =
+                currentCritChance * currentDamagePerHit * 1.75 / (2.0 * currentAttackRate / 1000);
+        }
+
+        /// <summary>
+        /// Checks if the given value for damage is higher than the current record and updates accordingly.
+        /// </summary>
+        /// <param name="dmg">Value to be compared with current record.</param>
+        private void UpdateMaxDamage(int dmg)
+        {
+            if (dmg > currentRecord)
+            {
+                currentRecord = dmg;
+                MaxDamageValue.Content = currentRecord;
+            }
+        }
+
+        /// <summary>
+        /// Event handler to increase attack power.
+        /// </summary>
+        /// <param name="sender">Button object that fired the event.</param>
+        /// <param name="e"></param>
         private void IncreaseAP(object sender, RoutedEventArgs e)
         {
-            currentAP += 1;
-            UpdateDamageConstraints();
-            HideIncreaseButtons();
-
-            Log.Text = "Attack Power upgraded by 1.\n";
+            if (availablePoints > 0)
+            {
+                currentAP++;
+                availablePoints--;
+                ValueAP.Content = currentAP;
+                UpdateDamagePerHit();
+                if (availablePoints == 0)
+                {
+                    HideIncreaseButtons();
+                }
+                AppendToLog("Attack Power upgraded by 1.", "\n");
+            }
+            
         }
 
+        /// <summary>
+        /// Event handler to increase critical rating.
+        /// </summary>
+        /// <param name="sender">Button object that fired the event.</param>
+        /// <param name="e"></param>
         private void IncreaseCR(object sender, RoutedEventArgs e)
         {
-            currentCR += 1;
-            HideIncreaseButtons();
-            Log.Text = "Critical Strike Rating upgraded by 1.\n";
+            if (availablePoints > 0)
+            {
+                currentCR++;
+                availablePoints--;
+                ValueCR.Content = currentCR;
+                if (availablePoints == 0)
+                {
+                    HideIncreaseButtons();
+                }
+                AppendToLog("Critical Strike Rating upgraded by 1.", "\n");
+            }
         }
 
+        /// <summary>
+        /// Event handler to increase haste rating.
+        /// </summary>
+        /// <param name="sender">Button object that fired the event.</param>
+        /// <param name="e"></param>
         private void IncreaseHR(object sender, RoutedEventArgs e)
         {
-            currentHR += 1;
-            UpdateAttackRate();
-            HideIncreaseButtons();
-            Log.Text = "Haste Rating upgraded by 1.\n";
+            if (availablePoints > 0)
+            {
+                currentHR++;
+                availablePoints--;
+                ValueHR.Content = currentHR;
+                UpdateAttackRate();
+                if (availablePoints == 0)
+                {
+                    HideIncreaseButtons();
+                }
+                AppendToLog("Haste Rating upgraded by 1.", "\n");
+            }
         }
 
         private void HideIncreaseButtons()
         {
-            IncrBttnAP.Visibility = Visibility.Hidden;
-            IncrBttnCR.Visibility = Visibility.Hidden;
-            IncrBttnHR.Visibility = Visibility.Hidden;
-            IncrBttnAP.IsEnabled = false;
-            IncrBttnCR.IsEnabled = false;
-            IncrBttnHR.IsEnabled = false;
+                IncrBttnAP.Visibility = Visibility.Hidden;
+                IncrBttnCR.Visibility = Visibility.Hidden;
+                IncrBttnHR.Visibility = Visibility.Hidden;
+                IncrBttnAP.IsEnabled = false;
+                IncrBttnCR.IsEnabled = false;
+                IncrBttnHR.IsEnabled = false;
         }
 
         private void ShowIncreaseButtons()
@@ -240,17 +323,31 @@ namespace Beatbox
             IncrBttnHR.IsEnabled = true;
         }
 
+        private void AppendToLog(Object text, String seperator)
+        {
+            StringBuilder sb = new StringBuilder(Log.Text);
+            sb.Append(text);
+            sb.Append(seperator);
+            Log.Text = sb.ToString();
+            ScrollViewer.ScrollToEnd();
+        }
+
         private void StartBeatbox(object sender, RoutedEventArgs e)
         {
-            StartWorker();
-
-            Log.Text = "Starting to hit stuff...\n";
+            if (!worker.IsBusy)
+            {
+                worker.RunWorkerAsync();
+                AppendToLog("Starting to hit stuff...", "\n");
+            }
         }
 
         private void StopBeatbox(object sender, RoutedEventArgs e)
         {
-            worker.CancelAsync();
-            Log.Text = "Enough hitting, stopping now.\n";
+            if (worker.IsBusy)
+            {
+                worker.CancelAsync();
+                AppendToLog("Enough hitting, going to stop now.", "\n");
+            }
         }
     }
 }
